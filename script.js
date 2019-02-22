d4033d28c06a1445997595ea2ad1abc733102288
WillDRuss <willrussell90@hotmail.com>
2019-02-22 15:28:28 +0000

    updating JS file for ES6 syntax

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var css =  document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var hexArr = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
-var enter = document.getElementById("enter");
+const css =  document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+const hexArr = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+const enter = document.getElementById("enter");
 
 function setGradient() {
 	body.style.background = 
@@ -15,24 +15,24 @@ function setGradient() {
 }
 
 function generateRandColors() {
-	var rCArr1 = [];
-	var rCArr2 = [];
-	for (var i=0; i<6; i++) {
-		var x = Math.round(Math.random() * 15);
-		var y = Math.round(Math.random() * 15);
+	let rCArr1 = [];
+	let rCArr2 = [];
+	let randomRgb1;
+	let randomRgb2;
+	for (let i=0; i<6; i++) {
+		let x = Math.round(Math.random() * 15);
+		let y = Math.round(Math.random() * 15);
 		rCArr1.push(hexArr[x]);
 		rCArr2.push(hexArr[y]);
-		var randomRgb1 = "#" 
+		randomRgb1 = "#" 
 		+ rCArr1[0] + rCArr1[1] + rCArr1[2]
 		+ rCArr1[3] + rCArr1[4] + rCArr1[5];
-		var randomRgb2 = "#" 
+		randomRgb2 = "#" 
 		+ rCArr2[0] + rCArr2[1] + rCArr2[2]
 		+ rCArr2[3] + rCArr2[4] + rCArr2[5];
 	}
 	color1.value = randomRgb1;
 	color2.value = randomRgb2;
-	console.log(randomRgb1);
-	console.log(randomRgb2);
 	setGradient();
 }
 
